f7430a163b3e4b147772f6755ef6b313b13012b6
song <309222152@qq.com>
2019-10-23 19:29:25 +0800

    转换成小写字母

---
+++ scr/pers/song/leetcode/editor/cn/ToLowerCase.java
@@ -0,0 +1,41 @@
+//实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入: "Hello"
+//输出: "hello" 
+//
+// 示例 2： 
+//
+// 
+//输入: "here"
+//输出: "here" 
+//
+// 示例 3： 
+//
+// 
+//输入: "LOVELY"
+//输出: "lovely"
+// 
+// Related Topics 字符串
+//有现成的白用白不用 2019-10-23 OK
+  
+package pers.song.leetcode.editor.cn;
+public class ToLowerCase{
+    public static void main(String[] args) {
+        Solution solution = new ToLowerCase().new Solution();
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
