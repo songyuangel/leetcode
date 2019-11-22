02e8035424028e68d7c73c9728feb923f8582308
songyuangel <309222152@qq.com>
2019-11-22 15:07:35 +0800

    831 隐藏个人信息

---
+++ scr/pers/song/leetcode/editor/cn/MaskingPersonalInformation.java
@@ -0,0 +1,123 @@
+//给你一条个人信息 string S，它可能是一个邮箱地址，也可能是一个电话号码。 
+//
+// 我们将隐藏它的隐私信息，通过如下规则: 
+//
+// 
+//
+// <u>1. 电子邮箱</u> 
+//
+// 定义名称 <name> 是长度大于等于 2 （length ≥ 2），并且只包含小写字母 a-z 和大写字母 A-Z 的字符串。 
+//
+// 电子邮箱地址由名称 <name> 开头，紧接着是符号 <font face="Menlo, Monaco, Consolas, Courier New, monospace">'@'</font>，后面接着一个名称 <name>，再接着一个点号 '.'，然后是一个名称 <name>。 
+//
+// 电子邮箱地址确定为有效的，并且格式是 "name1@name2.name3"。 
+//
+// 为了隐藏电子邮箱，所有的名称 <name> 必须被转换成小写的，并且第一个名称 <name> 的第一个字母和最后一个字母的中间的所有字母由 5 个 '*' 代替。 
+//
+// 
+//
+// <u>2. 电话号码</u> 
+//
+// 电话号码是一串包括数字 0-9，以及 {'+', '-', '(', ')', ' '} 这几个字符的字符串。你可以假设电话号码包含 10 到 13 个数字。 
+//
+// 电话号码的最后 10 个数字组成本地号码，在这之前的数字组成国际号码。注意，国际号码是可选的。我们只暴露最后 4 个数字并隐藏所有其他数字。 
+//
+// 本地号码是有格式的，并且如 "***-***-1111" 这样显示，这里的 1 表示暴露的数字。 
+//
+// 为了隐藏有国际号码的电话号码，像 "+111 111 111 1111"，我们以 "+***-***-***-1111" 的格式来显示。在本地号码前面的 '+' 号和第一个 '-' 号仅当电话号码中包含国际号码时存在。例如，一个 12 位的电话号码应当以 "+**-" 开头进行显示。 
+//
+// 注意：像 "("，")"，" " 这样的不相干的字符以及不符合上述格式的额外的减号或者加号都应当被删除。 
+//
+// 
+//
+// 最后，将提供的信息正确隐藏后返回。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入: "LeetCode@LeetCode.com"
+//输出: "l*****e@leetcode.com"
+//解释： 
+//所有的名称转换成小写, 第一个名称的第一个字符和最后一个字符中间由 5 个星号代替。
+//因此，"leetcode" -> "l*****e"。
+// 
+//
+// 示例 2： 
+//
+// 输入: "AB@qq.com"
+//输出: "a*****b@qq.com"
+//解释: 
+//第一个名称"ab"的第一个字符和最后一个字符的中间必须有 5 个星号
+//因此，"ab" -> "a*****b"。
+// 
+//
+// 示例 3： 
+//
+// 输入: "1(234)567-890"
+//输出: "***-***-7890"
+//解释: 
+//10 个数字的电话号码，那意味着所有的数字都是本地号码。
+// 
+//
+// 示例 4： 
+//
+// 输入: "86-(10)12345678"
+//输出: "+**-***-***-5678"
+//解释: 
+//12 位数字，2 个数字是国际号码另外 10 个数字是本地号码 。
+// 
+//
+// 
+//
+// 注意: 
+//
+// 
+// S.length <= 40。 
+// 邮箱的长度至少是 8。 
+// 电话号码的长度至少是 10。 
+// 
+// Related Topics 字符串
+//2019-11-22 ok
+  
+  package pers.song.leetcode.editor.cn;
+  public class MaskingPersonalInformation{
+      public static void main(String[] args) {
+           Solution solution = new MaskingPersonalInformation().new Solution();
+           System.out.println(solution.maskPII("+(501321)-50-23431"));
+
+      }
+      
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String maskPII(String S) {
+        int atIndex = S.indexOf('@');
+        StringBuilder result = new StringBuilder();
+        if(atIndex > 0){        //电子邮箱
+            String name1 = S.substring(0,atIndex);
+            result.append(name1.substring(0,1));
+            result.append("*****");
+            result.append(name1.substring(name1.length()-1,name1.length()));
+            result.append(S.substring(atIndex,S.length()));
+
+        }else{                  //手机号码
+            String telnum = S.replace("+","").replace("-","").replace(" ","").replace("(","").replace(")","");
+            if(telnum.length() == 10){  //本地号码
+                result.append("***-***-");
+
+            }else{                      //国际号码
+                result.append("+");
+                for(int i = 0 ; i < telnum.length() - 10 ; i++){
+                    result.append("*");
+                }
+                result.append("-***-***-");
+            }
+            result.append(telnum.substring(telnum.length()-4,telnum.length()));
+        }
+        return result.toString().toLowerCase();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
